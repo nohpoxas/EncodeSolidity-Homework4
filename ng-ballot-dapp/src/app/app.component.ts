c75bc90a7320baefaf590744867deea136220bf0
JoseAmbiel <63266094+JoseAmbiel@users.noreply.github.com>
2022-11-27 13:21:16 +0100

    Update app.component.ts
    classes notes

--- ng-ballot-dapp/src/app/app.component.ts
+++ ng-ballot-dapp/src/app/app.component.ts
@@ -1,23 +1,88 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ethers } from 'ethers';
+import tokenJson from '../assets/MyToken.json';
+
+// claimTokensDto should be preferably in another file
+export class claimTokensDto {
+  constructor(public address: string) {}
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   wallet: ethers.Wallet | undefined;
+  provider: ethers.providers.Provider;
+  tokenAddress: string | undefined;
+  tokenContract: ethers.Contract | undefined;
 
-  constructor() { }
+  ethBalance: number | undefined;
+  tokenBalance: number | undefined;
+  votingPower: number | undefined;
+
+  constructor(private http: HttpClient) {
+    this.provider = ethers.getDefaultProvider('goerli');
+    this.http
+      .get<any>('http://localhost:3000/token-address')
+      .subscribe((ans) => {
+        this.tokenAddress = ans.result;
+      });
+  }
 
   createWallet() {
-
+    this.wallet = ethers.Wallet.createRandom().connect(this.provider);
+    if (this.tokenAddress) {
+      this.tokenContract = new ethers.Contract(
+        this.tokenAddress,
+        tokenJson.abi,
+        this.wallet
+      );
+    }
+    this.updateValue();
   }
 
-  importWallet(seedOrPrivateKey: string) {
-
+  async updateValue() {
+    this.wallet?.getBalance().then((balanceBN) => {
+      this.ethBalance = parseFloat(ethers.utils.formatEther(balanceBN));
+    });
+    if (this.tokenContract) {
+      this.tokenContract['balanceOf'](this.wallet?.address).then(
+        (balanceBN: ethers.BigNumberish) => {
+          this.tokenBalance = parseFloat(ethers.utils.formatEther(balanceBN));
+        }
+      );
+      this.tokenContract['getVotes'](this.wallet?.address).then(
+        (votingPowerBN: ethers.BigNumberish) => {
+          this.votingPower = parseFloat(
+            ethers.utils.formatEther(votingPowerBN)
+          );
+        }
+      );
+    }
   }
 
+  importWallet(privateKey: string) {
+    // TODO (optional): make this.wallet to be imported from private key
+    this.updateValue();
+  }
+
+  requestTokens() {
+    const body = new claimTokensDto(this.wallet?.address ?? '');
+    this.http
+      .post<any>('http://localhost:3000/claim-tokens', body)
+      .subscribe((ans) => {
+        const txHash = ans.result;
+        // TODO: const tx = this.provider.getTransaction(txHash);
+        // TODO: tx.wait()
+        // TODO: aster tx confirms we can call updateValues again
+      });
+  }
+
+  connectBallot() {
+    //TODO: connect a ballot instance attached to this address
+    //TODO: fetch information of that ballot to be displayed in the page
+  }
 }
