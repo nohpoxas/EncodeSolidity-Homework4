2688ea3258ed1d06b5c2898fface0792f251a742
JoseAmbiel <63266094+JoseAmbiel@users.noreply.github.com>
2022-11-27 12:52:51 +0100

    Added the corsOptions
    In case of problems communicating with the frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -13,6 +13,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
