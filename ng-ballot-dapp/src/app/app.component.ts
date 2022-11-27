9b46917a35d9ec3a1f820c96a5625a21226cc81b
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-11-27 13:05:26 +0100

    Init main frontend app page

--- ng-ballot-dapp/src/app/app.component.ts
+++ ng-ballot-dapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ballot-dapp';
+
+  wallet: ethers.Wallet | undefined;
+
+  constructor() { }
+
+  createWallet() {
+
+  }
+
+  importWallet(seedOrPrivateKey: string) {
+
+  }
+
 }
